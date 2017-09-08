c51332e10685452bdd8b74d1bd85426fc6304e9a
河河海 <riverhohai@gmail.com>
2017-09-08 09:37:17 +0800

    base development env adn config

---
+++ log.md
@@ -0,0 +1,10 @@
+- git init
+- create gitignore
+- yarn init
+- create express app
+- set development env (set env cross-env)
+- get babel
+- config webpack
+- use nodemon
+- create editorconfig
+- use eslint
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+
+const PROT = process.env.PROT || 3000
+
+app.listen(PROT, err => {
+  if (err) {
+    throw err
+  } else {
+    console.log(`
+      ---
+      Server runing in prot: ${PROT}
+      ---
+      Runing on ${process.env.NODE_ENV}
+      ---
+    `)
+  }
+})
